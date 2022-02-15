00000d4bf9017801bae983c4009458578e4e2aed
Corey Helton <corey@coreyhelton.com>
2022-02-15 07:32:22 -0600

    Updated menu icons

--- src/components/AppBar/MenuItems.jsx
+++ src/components/AppBar/MenuItems.jsx
@@ -1,49 +1,44 @@
 import React from 'react';
-import {
-  IoHomeOutline,
-  IoCashOutline,
-  IoLogoElectron,
-  IoGlobeOutline,
-  IoBulbOutline,
-} from 'react-icons/io5';
-
-import { HiMailOpen } from 'react-icons/hi';
+import { HiHome, HiInformationCircle, HiMailOpen } from 'react-icons/hi';
+import { FaSign } from 'react-icons/fa';
+import { GiCycle } from 'react-icons/gi';
+import { RiMoneyDollarCircleFill } from 'react-icons/ri';
 
 const MenuItems = [
   {
     title: 'Home',
     path: '/',
-    icon: <IoGlobeOutline size="1.5rem" />,
+    icon: <HiHome size="2rem" />,
     cName: 'nav-text',
   },
   {
     title: 'About',
     path: '/about',
-    icon: <IoBulbOutline size="1.5rem" />,
+    icon: <HiInformationCircle size="2rem" />,
     cName: 'nav-text',
   },
   {
     title: 'Properties Avilable',
     path: '/properties-availble',
-    icon: <IoHomeOutline size="1.5rem" />,
+    icon: <FaSign size="2rem" />,
     cName: 'nav-text',
   },
   {
     title: 'Our Process',
     path: '/our-process',
-    icon: <IoLogoElectron size="2.5rem" />,
+    icon: <GiCycle size="1.8rem" />,
     cName: 'nav-text',
   },
   {
     title: 'Get an Offer',
     path: '/get-offer',
-    icon: <IoCashOutline size="2.5rem" />,
+    icon: <RiMoneyDollarCircleFill size="2rem" />,
     cName: 'nav-text',
   },
   {
     title: 'Contact Us',
     path: '/contact',
-    icon: <HiMailOpen size="2.5rem" />,
+    icon: <HiMailOpen size="2rem" />,
     cName: 'nav-text',
   },
 ];
